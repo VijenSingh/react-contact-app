3cf6c46e1dff7b56d0f2c31e3c9641d278f0164f
vijendra <vijendra@Vijendras-MacBook-Air.local>
2022-06-07 00:54:52 +0530

    create componect like addcontact contactList Etc and import to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import React from "react";
+import Header from "./components/Header";
+import AddContact from "./components/AddContact";
+import ContactList from "./components/ContactList";
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="ui container">
+      <Header />
+      <AddContact />
+      <ContactList />
+    </div>
+  );
 }
 
 export default App;
